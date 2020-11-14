a6d61d7a92705a36b0d577d2c68f24a655d795ef
alexscorpy <scorpy2013@gmail.com>
2020-11-14 21:48:28 +0200

    lab-05-stack

---
+++ include/stack1.hpp
@@ -0,0 +1,66 @@
+// Copyright 2020 Your Name <your_email>
+
+#ifndef TEMPLATE_STACK1_HPP
+#define TEMPLATE_STACK1_HPP
+#include <stdexcept>
+#include <utility>
+
+template <class T>
+struct Element {
+  T value; // значение элемента
+  Element<T> *prev, *next; // ссылки на предыдущий и следующий элемент
+};
+// некопируемый перемещаемый шаблон класса stack с методами pop, push, head
+template <typename T>
+class Stack1 {
+ public:
+  Stack1() = default; // конструктор по умолчанию
+  void push(const T &value); // вставка элемента
+  void push(T &&value); // вставка элемента
+  void pop(); // уничтожение элемента
+  const T &head() const; // доступ к вершине стека
+  ~Stack1(); // деструктор
+
+ private:
+  Element<T> *HEAD; // верхушка стека
+  Element<T> *TAIL; // "хвост" стека
+};
+template <typename T>
+void Stack1<T>::push(const T &value) {
+  auto *new_element = new Element<T>{value, HEAD};
+  HEAD = new_element;
+}
+
+template <typename T>
+void Stack1<T>::push(T &&value) {
+  auto *new_element = new Element<T>{std::forward<T>(value), HEAD};
+  HEAD = new_element;
+}
+
+template <typename T>
+void Stack1<T>::pop() {
+  if (HEAD) {
+    auto *FIRST = HEAD;
+    HEAD = HEAD->previous; // теперь "вершина" стека - это предыдущий элемент
+    delete FIRST;
+  } else {
+    throw std::out_of_range("Stack is empty!");
+  }
+}
+template <typename T>
+const T &Stack1<T>::head() const {
+  if (!HEAD) {
+    throw std::out_of_range("Stack is empty!");
+  }
+  return HEAD->value; // возвращаем значение элемента
+}
+
+template <typename T>
+Stack1<T>::~Stack1() {
+  while (HEAD) {
+    auto *current_element = HEAD; // текущий элемент - теперь "вершина"
+    HEAD = HEAD->prev; // "вершина" является предыдущим элементом
+    delete current_element; // уничтожаем текщий элемент
+  }
+}
+#endif  // TEMPLATE_STACK1_HPP
---
+++ include/stack2.hpp
@@ -0,0 +1,58 @@
+// Copyright 2020 Your Name <your_email>
+
+#ifndef TEMPLATE_STACK2_HPP
+#define TEMPLATE_STACK2_HPP
+#include <iostream>
+#include <type_traits>
+template <class T>
+struct Element {
+  T value;
+  Element<T> *prev, *next;
+};
+template <typename T>
+class Stack2
+{
+ public:
+  template <typename ... Args>
+  void push_emplace(Args&&... value);
+  void push(T&& value);
+  const T& head() const;
+  T pop();
+ private:
+  Element<T> *HEAD;
+  Element<T> *TAIL;
+};
+
+template <typename T>
+void Stack2<T>::push(T &&value) {
+  if (std::is_copy_constructible<T>::value ||
+      std::is_copy_assignable<T>::value) {
+    throw std::bad_typeid();
+  }
+  auto *new_element = new Element<T>{std::forward<T>(value), HEAD};
+  HEAD = new_element;
+}
+
+template <typename T>
+T Stack2<T>::pop() {
+  if (!HEAD) {
+    throw std::out_of_range("Empty Stack!!!");
+  }
+  auto *FIRST = HEAD;
+  HEAD = HEAD->previous;
+  T value = std::move(FIRST->value);
+  delete FIRST;
+  return value;
+}
+
+template <typename T>
+template <typename... Args>
+void Stack2<T>::push_emplace(Args &&... value) {
+  if (std::is_copy_constructible<T>::value ||
+      std::is_copy_assignable<T>::value) {
+    throw std::bad_typeid();
+  }
+  auto *new_element = new Element<T>{{std::forward<Args>(value)...}, HEAD};
+  HEAD = new_element;
+}
+#endif  // TEMPLATE_STACK2_HPP
